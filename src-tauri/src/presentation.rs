c48d3b65721df5795d5a4d40dd66434f9f832092
stkii <229047545+stkii@users.noreply.github.com>
2026-03-12 11:33:57 +0900

    chore: typo

--- src-tauri/src/presentation.rs
+++ src-tauri/src/presentation.rs
@@ -2,7 +2,7 @@ mod commands;
 
 pub(crate) fn attach_handlers(builder: tauri::Builder<tauri::Wry>) -> tauri::Builder<tauri::Wry> {
     builder.invoke_handler(tauri::generate_handler![commands::build_numeric_dataset::build_numeric_dataset,
-                                                    commands::creare_dataset_cache::clear_numeric_dataset_cache,
+                                                    commands::clear_numeric_dataset_cache::clear_numeric_dataset_cache,
                                                     commands::get_sheets::get_sheets,
                                                     commands::parse_table::parse_table,
                                                     commands::run_analysis::run_analysis,])
